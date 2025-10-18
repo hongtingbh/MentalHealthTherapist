717e0b0dfb300c0d9005c9e19eecd6359bc72429
Brandon Ho <hongting.bh@gmail.com>
2025-10-18 04:25:39 +0000

    the authorized domains seem correct. Here's what I have: localhost	Defau

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,10 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 
+const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  authDomain: `${projectId}.firebaseapp.com`,
+  projectId: projectId,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
