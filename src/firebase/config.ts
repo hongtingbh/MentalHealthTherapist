43fc74bdfaa4b9f6ba64c4875ad50d4eadc6a110
Brandon Ho <hongting.bh@gmail.com>
2025-11-05 21:36:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6533424125-c5338",
+  "appId": "1:133592228073:web:ea3241e63decd9f7355078",
+  "apiKey": "AIzaSyBlBqsQCiPr-4j1a5YHL-ACWyhXUszoAvU",
+  "authDomain": "studio-6533424125-c5338.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "133592228073"
+};
